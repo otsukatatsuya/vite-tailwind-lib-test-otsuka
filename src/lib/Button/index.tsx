e5b5d87dbabb001c65dbce22915a73a9315c18a9
otsukatatsuya <tatsuya.otsuka@ncdc.co.jp>
2023-11-04 10:05:59 +0900

    Button Componen実装

---
+++ src/lib/Button/index.tsx
@@ -0,0 +1,23 @@
+import { FC, ButtonHTMLAttributes } from "react";
+import cn from "clsx";
+import styles from "./index.module.css";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: "primary" | "secondary" | "tertiary";
+}
+
+const Button: FC<ButtonProps> = ({
+  variant = "primary",
+  children,
+  className,
+  ...props
+}) => {
+  const rootClassName = cn(styles.root, styles[variant], className);
+  return (
+    <button className={rootClassName} {...props}>
+      {children}
+    </button>
+  );
+};
+
+export default Button;
